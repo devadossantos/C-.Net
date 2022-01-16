ace1097d4e8300be466ca7877601dab58e803d93
Perkles <anisio.gustavo@outlook.com>
2022-01-16 12:59:23 -0300

    atualizacao

---
+++ limitar.acesso/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Teste
+{
+public class LimitacaoUsuario
+{
+    static void Main(string[] args)
+{
+      string usuario = "anisio.santos";     // usuario de acesso
+      int senh = 12345;                       // senha de acesso
+      string respuser;
+      int respsenha;
+        inicio:
+        Console.Clear();
+        Console.WriteLine("################");
+        Console.WriteLine("ACESSO RESTRITO");
+        Console.WriteLine("################\n");
+        Console.Write("Qual seu usuario? ");
+        respuser = (Console.ReadLine());
+        Console.Write("Qual sua senha? ");
+        respsenha = int.Parse(Console.ReadLine());
+        
+        while(respsenha != senh && respuser != usuario) {                 
+
+         Console.WriteLine("ACESSO NAO AUTORIZADO");                  
+        Console.ReadLine();
+         goto inicio;                                              // volte ao inicio
+        }
+        
+        Console.WriteLine("AUTORIZADO");                            
+        Console.WriteLine("ACESSO AUTORIZADO");             
+        Console.ReadLine();
+                       
+}
+}
+}
